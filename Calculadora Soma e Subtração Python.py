6d55a6b7fc21067396ab26046c4ffaa3bb906696
Ricardo Dib <henrique-dib@hotmail.com>
2023-09-01 12:19:07 -0300

    calculadora

---
+++ Calculadora Soma e Subtração Python.py
@@ -0,0 +1,15 @@
+num1 = input('Digite o primeiro número: ')
+num2 = input('digite o segundo número: ')
+
+resultado = int(num1) + int(num2)
+
+
+print(resultado)
+
+
+num3 = input('Digite o primeiro número: ')
+num4 = input('digite o segundo número: ')
+
+resultado = int(num3) - int(num4)
+
+print(resultado)
